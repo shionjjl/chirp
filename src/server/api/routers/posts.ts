128bfb0cb2f2040ca5892096f8f08934b2d23722
Jason Jiasheng Liu <shion5548@gmail.com>
2025-09-30 02:38:21 +1000

    feat: next image tag, style up

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,73 @@
+import { clerkClient, type User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+};
+
+export const postsRouter = createTRPCRouter({
+  // hello: publicProcedure
+  //   .input(z.object({ text: z.string() }))
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `Hello ${input.text}`,
+  //     };
+  //   }),
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const client = await clerkClient();
+
+    const users = (
+      await client.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).data.map(filterUserForClient);
+
+    console.log(users);
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Author for post ${post.id} not found`,
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
+  }),
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     return ctx.db.post.create({
+  //       data: {
+  //         content: input.name, authorId: "authorId",
+  //       },
+  //     });
+  //   }),
+
+  // getLatest: publicProcedure.query(async ({ ctx }) => {
+  //   const post = await ctx.db.post.findFirst({
+  //     orderBy: { createdAt: "desc" },
+  //   });
+
+  //   return post ?? null;
+  // }),
+});
